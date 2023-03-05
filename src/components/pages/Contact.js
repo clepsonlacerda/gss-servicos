40a715a34a72835dbea108f4eeb8f0b9801180f7
IscoClepson <clepsonsantos9@gmail.com>
2023-03-05 14:25:07 -0300

    adicionado uma mensagem após o envio da mensagem na pagina de contato

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,13 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 import emailjs from '@emailjs/browser';
 import Footer from '../Footer'
 import './Contact.css'
 
 function Contact() {
+
+  const [showMessageSuccess, setShowMessageSuccess] = useState(false);
+  const [showMessageError, setShowMessageError] = useState(false);
 
   const form = useRef();
 
@@ -13,9 +16,17 @@ function Contact() {
 
     emailjs.sendForm('service_5a6jw0o', 'template_7maejno', form.current, 'v3YgYnGubRZYC2SLJ')
       .then((result) => {
-        console.log(result.text);
+        setShowMessageSuccess(true);
+        
+        setTimeout(function() {
+          setShowMessageSuccess(false);
+        }, 3000); 
       }, (error) => {
-        console.log(error.text);
+        setShowMessageError(true);
+        
+        setTimeout(function() {
+          setShowMessageError(false);
+        }, 3000); 
       });
       e.target.reset();
   };
@@ -29,12 +40,26 @@ function Contact() {
           <h2>NOS CONTACTE</h2>
 
           <form ref={form} onSubmit={sendEmail} >
+
+            {showMessageSuccess &&
+              <div className="message-success">
+                Mensagem Enviada!
+              </div>
+            }
+
+            {showMessageError &&
+              <div className="message-error">
+                Erro ao enviar Messagem!
+              </div>
+            }
+
             <input type="text" placeholder='INFORME SEU NOME' name='user_name' required />
             <input type="email" placeholder='INFORME SEU EMAIL' name='user_email' required />
             <input type="text" placeholder='INFORME O ASSUNTO' name='subject' required />
             <textarea name="message" cols="30" rows="5"></textarea>
 
             <button type='submit'>Enviar Mensagem</button>
+
           </form>
         </div>
         <div className="container-footer">
